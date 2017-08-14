8e5ab1177f8400df368d3e7bcab5f1703f0b10ab
William W. Chiang <william.pharmd@gmail.com>
2017-08-13 19:29:42 -0500

    test now

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -7,6 +7,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Test commit
+    
     // Our strings
     let story1 = "Your car has blown a tire on a winding road in the middle of nowhere with no cell phone reception. You decide to hitchhike. A rusty pickup truck rumbles to a stop next to you. A man with a wide brimmed hat with soulless eyes opens the passenger door for you and asks: \"Need a ride, boy?\"."
     let answer1a = "I\'ll hop in. Thanks for the help!"
